a24fb9ff1a2757a8701239d5e66736711165a7da
Parth-05 <parthmarathe5@gmail.com>
2024-02-05 02:32:44 -0800

    Updated store.js

--- Full_Application/client/src/redux/store.js
+++ Full_Application/client/src/redux/store.js
@@ -1,17 +1,24 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+// import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { thunk } from 'redux-thunk';
 import { predictPriceReducer } from './reducers/PredictPriceReducer';
-import {composeWithDevTools} from "redux-devtools-extension";
+// import {composeWithDevTools} from "redux-devtools-extension";
 
 const reducers = combineReducers({
     predictedPrice: predictPriceReducer
 });
 
-const middleware = [thunk];
+// const middleware = [thunk];
 
-const store = createStore(
-    reducers,
-    composeWithDevTools(applyMiddleware(...middleware))
+// const store = createStore(
+//     reducers,
+//     composeWithDevTools(applyMiddleware(...middleware))
+// )
+
+const store = configureStore({
+    reducer: reducers,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+}
 )
 
 export default store;
